212ab8a5d2ad852f9281bbd52dc868b12c8cddc7
Benjamin Morris <benjijamorris@gmail.com>
2022-11-02 11:44:25 -0700

    add monai-compatible multiscale crop

--- aics_im2im/utils/multiscale_cropper.py
+++ aics_im2im/utils/multiscale_cropper.py
@@ -1,39 +1,51 @@
+from monai.transforms import RandomizableTransform
 import numpy as np
 
-
-import numpy as np
-
-class MultiScaleCropper:
-    def __init__(self, image_shape, roi_size: tuple, scales: list, num_samples: int):
-        roi_size = np.asarray(roi_size)
-        self.dims = len(image_shape)
-        self.max_start = np.asarray(image_shape) - roi_size
+class MultiScaleCrops(RandomizableTransform):
+    def  __init__(self, roi_size, num_samples, scales_dict):
+        self.roi_size = np.asarray(roi_size)
         self.num_samples = num_samples
-        self.scales = scales
+        self.scale_dict = scales_dict
+        self.reversed_scale_dict = {}
+        for k, v in scales_dict.items():
+            for v_item in v:
+                self.reversed_scale_dict[v_item] = k
+        assert 1  in self.scale_dict.keys()
+        
 
-        start_indices = self.generate_start_indices()
-        scaled_start_indices = self.generate_scaled_start_indices(start_indices)
-        self.slices = self.generate_multiscale_slices(scaled_start_indices)
+    def _generate_slice(self, start_coords, roi_size):
+        return [slice(None, None)] + [
+            slice(start, end)
+            for start, end in zip(start_coords, start_coords + roi_size)
+        ]
 
-    def generate_scaled_start_indices(self, start_indices):
+    def generate_multiscale_slices(self, start_indices):
         return {
-            s: (start_indices//s).astype(int) for s in self.scales
+            s: [
+                self._generate_slice(start_indices[s][i],(self.roi_size // s).astype(int))
+                for i in range(start_indices[s].shape[0])
+            ]
+            for s in start_indices.keys()
         }
-
-    def generate_start_indices(self):
-        return np.asarray([np.random.randint(self.max_start) for _ in range(self.num_samples)])
-
-    def generate_slice(self, start_coords, roi_size):
-        return [slice(None, None)] + [slice(start, end) for start, end in zip(start_coords, start_coords + roi_size)]
-
-    def generate_multiscale_slices(self, start_indices, roi_size):
-        return {
-            s: [self.generate_slice(start_indices[s][i], (roi_size//s).astype(int)) for i in range(start_indices.shape[0])] for  s in self.scales
-        }
-
-    def __call__(self, image, metadata):
-        patches ={}
-        for key, data in image.items():
-            patches[key] = [data[scale_slice] for scale_slice in self.slices[metadata[key]['patch_ratio']]]
+        
+        
+    def generate_slices(self, image_dict):
+        max_shape = np.asarray(image_dict[self.scale_dict[1][0]].shape[-3:])
+        max_start_indices = max_shape - self.roi_size
+        start_indices = np.asarray(
+            [self.R.randint(max_start_indices) for _ in range(self.num_samples)]
+        )
+        scaled_start_indices = {s: (start_indices // s).astype(int) for s in self.scale_dict.keys()}
+        return self.generate_multiscale_slices(scaled_start_indices)
+              
+        
+    def __call__(self, image_dict):
+        slices = self.generate_slices(image_dict)
+        patches = {}
+        for key, data in image_dict.items():
+            if 'meta_dict' in key:
+                continue
+            patches[key] = [
+                data[scale_slice] for scale_slice in slices[self.reversed_scale_dict[key]]
+            ]
         return patches
-
