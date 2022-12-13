668a6f6c50e30ef0230c70bdc27f59af7b913922
Benjamin Morris <benjijamorris@gmail.com>
2022-12-13 14:23:40 -0800

    add 2d aux head

--- aics_im2im/networks/aux_head.py
+++ aics_im2im/networks/aux_head.py
@@ -12,15 +12,16 @@ class AuxHead(torch.nn.Module):
         n_hr_convs=1,
         dropout=0.1,
         hr_skip_channels=0,
+        spatial_dims=3,
     ):
         super().__init__()
         self.resolution = resolution
         modules = []
         conv_input_channels = in_channels
         if resolution == "hr":
-            conv_input_channels //= 8
+            conv_input_channels //= 2 ** spatial_dims
             self.upsample = SubpixelUpsample(
-                spatial_dims=3,
+                spatial_dims=spatial_dims,
                 in_channels=in_channels,
                 out_channels=conv_input_channels,
             )
@@ -29,7 +30,7 @@ class AuxHead(torch.nn.Module):
                 # first convolution has to include skip connection from input
                 modules.append(
                     Convolution(
-                        spatial_dims=3,
+                        spatial_dims=spatial_dims,
                         in_channels=conv_input_channels + (hr_skip_channels) * (i == 0),
                         out_channels=conv_input_channels,
                         act=("leakyrelu", {"inplace": True, "negative_slope": 0.2}),
@@ -37,8 +38,12 @@ class AuxHead(torch.nn.Module):
                         dropout=dropout,
                     )
                 )
+        if spatial_dims == 3:
+            conv_fn = torch.nn.Conv3d
+        elif spatial_dims == 2:
+            conv_fn = torch.nn.Conv2d
         modules.append(
-            torch.nn.Conv3d(conv_input_channels, out_channels, kernel_size=1, padding=0)
+            conv_fn(conv_input_channels, out_channels, kernel_size=1, padding=0)
         )
         modules.append(final_act)
         self.aux_head = torch.nn.Sequential(*modules)
