e26f53b1c697a958161852e53fcd39a05195c7aa
Benjamin Morris <benjijamorris@gmail.com>
2023-02-01 12:06:20 -0800

    add omegaconf resolver for keys

--- aics_im2im/eval.py
+++ aics_im2im/eval.py
@@ -35,9 +35,10 @@ root = pyrootutils.setup_root(
 from typing import List, Tuple
 
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer
 from pytorch_lightning.loggers import LightningLoggerBase
+from serotiny.utils import kv_to_dict
 
 from aics_im2im import utils
 
@@ -98,6 +99,8 @@ def evaluate(cfg: DictConfig) -> Tuple[dict, dict]:
 
 @hydra.main(version_base="1.3", config_path="../configs", config_name="eval.yaml")
 def main(cfg: DictConfig) -> None:
+    OmegaConf.register_new_resolver("kv_to_dict", kv_to_dict)
+    OmegaConf.register_new_resolver("eval", eval)
     evaluate(cfg)
 
 
