4ba72c16720729c86c55cea17fd838786c1843e0
Benjamin Morris <benjijamorris@gmail.com>
2023-02-01 12:06:20 -0800

    add omegaconf resolver for keys

--- aics_im2im/eval.py
+++ aics_im2im/eval.py
@@ -43,7 +43,29 @@ from serotiny.utils import kv_to_dict
 from aics_im2im import utils
 
 log = utils.get_pylogger(__name__)
+from omegaconf import DictConfig, ListConfig, OmegaConf
 
+
+def kv_to_dict(kv: ListConfig) -> DictConfig:
+    """
+    Parameters
+    ----------
+    kv: ListConfig
+        ListConfig where every item is a nested list of the form [interpolated key, value]
+    Returns
+    -------
+    DictConfig of input
+    """
+    if isinstance(kv, ListConfig):
+        ret = {}
+        for item in kv:
+            assert (
+                len(item) == 2
+            ), f"Expected ListConfig item to have len 2, got {len(item)}"
+            ret[item[0]] = OmegaConf.to_container(item[1], resolve=True)
+    else:
+        raise TypeError("Config resolved with kv_to_dict must be ListConfig")
+    return OmegaConf.create(ret)
 
 @utils.task_wrapper
 def evaluate(cfg: DictConfig) -> Tuple[dict, dict]:
--- aics_im2im/train.py
+++ aics_im2im/train.py
@@ -45,7 +45,29 @@ from tempfile import TemporaryDirectory
 from pathlib import Path
 
 log = utils.get_pylogger(__name__)
+from omegaconf import DictConfig, ListConfig, OmegaConf
 
+
+def kv_to_dict(kv: ListConfig) -> DictConfig:
+    """
+    Parameters
+    ----------
+    kv: ListConfig
+        ListConfig where every item is a nested list of the form [interpolated key, value]
+    Returns
+    -------
+    DictConfig of input
+    """
+    if isinstance(kv, ListConfig):
+        ret = {}
+        for item in kv:
+            assert (
+                len(item) == 2
+            ), f"Expected ListConfig item to have len 2, got {len(item)}"
+            ret[item[0]] = OmegaConf.to_container(item[1], resolve=True)
+    else:
+        raise TypeError("Config resolved with kv_to_dict must be ListConfig")
+    return OmegaConf.create(ret)
 
 @utils.task_wrapper
 def train(cfg: DictConfig) -> Tuple[dict, dict]:
