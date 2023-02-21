67b3d4585d0807152b53b00830d89d45a2f4167a
Guilherme Pires <guilherme.pires@alleninstitute.org>
2023-02-21 14:43:14 -0800

    safeguard spharm loss import

--- aics_im2im/nn/losses/__init__.py
+++ aics_im2im/nn/losses/__init__.py
@@ -3,6 +3,10 @@ from .continuous_bernoulli import CBLogLoss
 from .cosine_loss import CosineLoss
 from .gan_loss import GANLoss
 from .gaussian_nll_loss import GaussianNLLLoss
-from .spharm_loss import SpharmLoss
 from .weibull import WeibullLogLoss
 from .weighted_mse_loss import WeightedMSELoss
+
+try:
+    from .spharm_loss import SpharmLoss
+except ModuleNotFoundError:
+    SpharmLoss = None
