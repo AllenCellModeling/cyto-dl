f9d9115c79d64eaa8e6126a189664dda654491fe
benjijamorris <54606172+benjijamorris@users.noreply.github.com>
2023-02-22 16:22:44 -0800

    Feature/embedseg inference speedup (#103)
    * allownaive labeling
    
    * update congif
    
    * check if list or listconfig
    
    * lint

--- aics_im2im/models/im2im/utils/omnipose.py
+++ aics_im2im/models/im2im/utils/omnipose.py
@@ -1,6 +1,7 @@
 # all code modified from https://github.com/kevinjohncutler/omnipose/blob/main/omnipose/core.py
 import warnings
 
+import dask
 import edt
 import numpy as np
 import torch
@@ -14,24 +15,31 @@ from cellpose_omni.core import (
 )
 from monai.data import MetaTensor
 from monai.transforms import Transform
+from omegaconf import ListConfig
 from omnipose.core import compute_masks, diameters, masks_to_flows
-from skimage.filters import gaussian
+from scipy.ndimage import find_objects
+from scipy.spatial import ConvexHull
+from skimage.filters import apply_hysteresis_threshold, gaussian
+from skimage.measure import label
+from skimage.morphology import binary_dilation, remove_small_holes
 from skimage.segmentation import expand_labels
 from skimage.transform import rescale, resize
 
 
 class OmniposePreprocessd(Transform):
-    def __init__(self, label_key, dim):
+    def __init__(self, label_keys, dim):
         super().__init__()
-        self.label_key = label_key
+        self.label_keys = (
+            label_keys if isinstance(label_keys, (list, ListConfig)) else [label_keys]
+        )
         self.dim = dim
 
     def __call__(self, image_dict):
         warnings.warn(
             "OmniPose preprocessing is slow, consider setting `persist_cache: True` in your experiment config"
         )
-        for _ in tqdm.tqdm(range(1)):
-            im = image_dict[self.label_key]
+        for key in tqdm.tqdm(self.label_keys):
+            im = image_dict[key]
             im = im.as_tensor() if isinstance(im, MetaTensor) else im
             numpy_im = im.numpy().squeeze()
 
@@ -54,7 +62,7 @@ class OmniposePreprocessd(Transform):
             out_im[3] = rough_distance
             out_im[4 : 4 + self.dim] = flows * 5.0  # weighted for loss function?
             out_im[4 + self.dim] = smooth_distance
-            image_dict[self.label_key] = out_im
+            image_dict[key] = out_im
         return image_dict
 
 
@@ -136,49 +144,135 @@ class OmniposeLoss:
         return loss
 
 
-def rescale_instance(im, seg):
-    seg = resize(seg, im.shape, order=0, anti_aliasing=False, preserve_range=True)
-    seg = expand_labels(seg, distance=3)
-    seg[im == 0] = 0
-    return seg
-
-
 # setting a high flow threshold avoids erroneous removal of masks that are fine.
 # debugging whether this is a training issue...
-def OmniposeClustering(
-    im,
-    mask_threshold=0,
-    rescale_factor=0.25,
-    min_object_size=100,
-    flow_threshold=1e8,
-    spatial_dim=3,
-):
+class OmniposeClustering:
     """Run clustering on downsampled version of flows, then use original resolution distance field
     to mask instance segmentations."""
-    device = im.device
-    im = im.detach().cpu().numpy()
-    flow = im[:spatial_dim]
-    dist = im[spatial_dim]
-    # bd = im[spatial_dim+1]
 
-    mask, p, tr, bounds = compute_masks(
-        rescale(
+    def __init__(
+        self,
+        mask_threshold=0,
+        rescale_factor=0.25,
+        min_object_size=100,
+        hole_size=0,
+        flow_threshold=1e8,
+        spatial_dim=3,
+        boundary_seg=True,
+        naive_label=False,
+    ):
+
+        self.mask_threshold = mask_threshold
+        self.rescale_factor = rescale_factor
+        self.min_object_size = min_object_size
+        self.hole_size = hole_size
+        self.flow_threshold = flow_threshold
+        self.spatial_dim = spatial_dim
+        self.boundary_seg = boundary_seg
+        self.naive_label = naive_label
+        self.clustering_function = self.do_naive_labeling if naive_label else self.get_mask
+
+    def rescale_instance(self, im, seg):
+        seg = resize(seg, im.shape, order=0, anti_aliasing=False, preserve_range=True)
+        seg = expand_labels(seg, distance=3)
+        seg[im == 0] = 0
+        return seg
+
+    def get_mask(self, flow, dist, device):
+        flow = rescale(
             flow,
-            [1] + [rescale_factor] * spatial_dim,
+            [1] + [self.rescale_factor] * self.spatial_dim,
             order=3,
             preserve_range=True,
             anti_aliasing=False,
-        ),
-        rescale(dist, rescale_factor, order=3, preserve_range=True, anti_aliasing=False),
-        # bd,
-        nclasses=4,
-        dim=spatial_dim,
-        use_gpu=True,
-        device=device,
-        min_size=min_object_size,
-        flow_threshold=flow_threshold,
-        boundary_seg=True,
-        mask_threshold=mask_threshold,
-    )
-    mask = rescale_instance(dist > mask_threshold, mask)
-    return mask
+        )
+        rescale_dist = rescale(
+            dist, self.rescale_factor, order=3, preserve_range=True, anti_aliasing=False
+        )
+        mask, p, tr, bounds = compute_masks(
+            flow,
+            rescale_dist,
+            nclasses=4,
+            dim=self.spatial_dim,
+            use_gpu=True,
+            device=device,
+            min_size=self.min_object_size,
+            flow_threshold=self.flow_threshold,
+            boundary_seg=self.boundary_seg,
+            mask_threshold=self.mask_threshold,
+        )
+        mask = self.rescale_instance(dist > self.mask_threshold, mask)
+        return mask
+
+    def pad_slice(self, s, padding, constraints):
+        new_slice = [slice(None, None, None)]
+        for slice_part, c in zip(s, constraints):
+            start = max(0, slice_part.start - padding)
+            stop = min(c, slice_part.stop + padding)
+            new_slice.append(slice(start, stop, None))
+        return new_slice
+
+    @dask.delayed
+    def get_separated_masks(self, flow_crop, mask_crop, dist_crop, ratio_threshold, device, crop):
+        if np.any(np.asarray(mask_crop.shape) < self.spatial_dim + 2):
+            return
+        area = np.sum(mask_crop)
+        if area < self.min_object_size:
+            return
+        c_hull = ConvexHull(np.asarray(list(zip(*np.where(mask_crop))))).volume
+        mask_crop = binary_dilation(mask_crop)
+        if c_hull / area > ratio_threshold:
+            flow_crop[:, ~mask_crop] = 0
+            dist_crop[~mask_crop] = dist_crop.min()
+            mask = self.get_mask(flow_crop, dist_crop, device)
+            return {"slice": tuple(crop[1:]), "mask": mask}
+        return {
+            "slice": tuple(crop[1:]),
+            "mask": remove_small_holes(
+                mask_crop > 0, area_threshold=self.hole_size, connectivity=self.spatial_dim
+            ),
+        }
+
+    def do_naive_labeling(self, flow, dist, device):
+        """label thresholded distance transform to get objects, then run clustering only on objects
+        that seem to be merged segmentations.
+
+        Useful for well-separated, round objects like nuclei
+        """
+        cellmask = apply_hysteresis_threshold(
+            dist, low=self.mask_threshold - 1, high=self.mask_threshold
+        )
+        naive_labeling = label(cellmask)
+        out_image = np.zeros_like(naive_labeling, dtype=np.uint16)
+        regions = find_objects(naive_labeling)
+        results = []
+        for val, region in enumerate(regions, start=1):
+            padded_crop = self.pad_slice(region, 5, naive_labeling.shape)
+            results.append(
+                self.get_separated_masks(
+                    flow[tuple(padded_crop)],
+                    naive_labeling[tuple(padded_crop[1:])] == val,
+                    dist[tuple(padded_crop[1:])],
+                    1.2,
+                    device,
+                    padded_crop,
+                )
+            )
+        results = dask.compute(*results)
+        highest_cell_idx = 0
+        for r in results:
+            if r is None:
+                continue
+            mask = r["mask"].astype(np.uint16)
+            mask[mask > 0] += highest_cell_idx
+            out_image[r["slice"]] += mask
+            highest_cell_idx += np.max(r["mask"])
+        return out_image
+
+    def __call__(self, im):
+        device = im.device
+        im = im.detach().cpu().numpy()
+        flow = im[: self.spatial_dim]
+        dist = im[self.spatial_dim]
+        mask = self.clustering_function(flow, dist, device)
+        return mask
