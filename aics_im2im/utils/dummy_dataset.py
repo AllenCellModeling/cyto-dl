e9e4aad10f592e081fbbf65afe6c3a191d2d94d4
Guilherme Pires <guilherme.pires@alleninstitute.org>
2023-02-27 18:34:28 -0800

    fix dummydataset instantiation

--- aics_im2im/utils/dummy_dataset.py
+++ aics_im2im/utils/dummy_dataset.py
@@ -4,14 +4,16 @@ from torch.utils.data import DataLoader, Dataset
 
 
 class DummyDatamodule(LightningDataModule):
-    def __init__(self, num_samples, batch_size, **shapes):
+    def __init__(self, num_samples, batch_size, shapes, **kwargs):
         super().__init__()
         self.shapes = shapes
         self.num_samples = num_samples
         self.batch_size = batch_size
 
     def get_dataloader(self):
-        return DataLoader(DummyDataset(self.num_samples, self.shapes), batch_size=self.batch_size)
+        return DataLoader(
+            DummyDataset(self.num_samples, **self.shapes), batch_size=self.batch_size
+        )
 
     def train_dataloader(self):
         return self.get_dataloader()
@@ -42,10 +44,13 @@ class DummyDataset(Dataset):
     def generate_img(self, k):
         if "seg" in k:
             im = torch.zeros(*self.shapes[k])
-            slicee = [slice(s // 2 - s // 4, s // 2 + s // 4, None) for s in self.shape[k]]
+            slicee = [slice(s // 2 - s // 4, s // 2 + s // 4, None) for s in self.shapes[k]]
             im[slicee] = 1
             return im
         return torch.randn(*self.shapes[k])
 
     def __getitem__(self, idx: int):
         return {k: self.generate_img(k) for k in self.shapes.keys()}
+
+    def __len__(self):
+        return self.num_samples
