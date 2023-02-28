e29fe47295bf69812375e48c221be91250b8ec78
Guilherme Pires <guilherme.pires@alleninstitute.org>
2023-02-28 14:46:08 -0800

    use omnipose as default experiment in sweep tests

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -9,27 +9,16 @@ overrides = ["logger=[]"]
 
 @RunIf(sh=True)
 @pytest.mark.slow
-def test_experiments(tmp_path):
-    """Test running all available experiment configs with fast_dev_run=True."""
-    command = [
-        startfile,
-        "-m",
-        "experiment=glob(*)",
-        "hydra.sweep.dir=" + str(tmp_path),
-        "++trainer.fast_dev_run=true",
-    ] + overrides
-    run_sh_command(command)
-
-
-@RunIf(sh=True)
-@pytest.mark.slow
 def test_hydra_sweep(tmp_path):
     """Test default hydra sweep."""
     command = [
         startfile,
         "-m",
+        "experiment=im2im/omnipose",
+        "data=test/omnipose",
+        "model=test/omnipose",
         "hydra.sweep.dir=" + str(tmp_path),
-        "model.optimizer.lr=0.005,0.01",
+        "+model.optimizer.lr=0.005,0.01",
         "++trainer.fast_dev_run=true",
     ] + overrides
 
@@ -43,12 +32,15 @@ def test_hydra_sweep_ddp_sim(tmp_path):
     command = [
         startfile,
         "-m",
+        "experiment=im2im/omnipose",
+        "data=test/omnipose",
+        "model=test/omnipose",
         "hydra.sweep.dir=" + str(tmp_path),
         "trainer=ddp_sim",
         "trainer.max_epochs=3",
-        "+trainer.limit_train_batches=0.01",
-        "+trainer.limit_val_batches=0.1",
-        "+trainer.limit_test_batches=0.1",
-        "model.optimizer.lr=0.005,0.01,0.02",
+        "+trainer.limit_train_batches=2",
+        "+trainer.limit_val_batches=2",
+        "+trainer.limit_test_batches=2",
+        "+model.optimizer.lr=0.005,0.01,0.02",
     ] + overrides
     run_sh_command(command)
