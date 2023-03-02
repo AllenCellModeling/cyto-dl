58b613eafa94c4fe5b9e5530d772b06283ff7355
Guilherme Pires <guilherme.pires@alleninstitute.org>
2023-03-01 19:19:12 -0800

    add main configs

--- aics_im2im/models/vae/__init__.py
+++ aics_im2im/models/vae/__init__.py
@@ -2,4 +2,5 @@ from .base_vae import BaseVAE
 from .image_vae import ImageVAE
 from .latent_loss_vae import LatentLossVAE
 from .o2_spharm_vae.o2_spharm_vae import O2SpharmVAE
+from .point_cloud_vae import PointCloudVAE
 from .tabular_vae import TabularVAE
