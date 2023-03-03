5e1728ab56e699f2f2db2c73a31e1952a83b72d3
Guilherme Pires <guilherme.pires@alleninstitute.org>
2023-03-03 12:09:52 -0800

    compartmentalize imports

--- aics_im2im/models/vae/__init__.py
+++ aics_im2im/models/vae/__init__.py
@@ -1,6 +1,17 @@
 from .base_vae import BaseVAE
 from .image_vae import ImageVAE
 from .latent_loss_vae import LatentLossVAE
-from .o2_spharm_vae.o2_spharm_vae import O2SpharmVAE
-from .point_cloud_vae import PointCloudVAE
 from .tabular_vae import TabularVAE
+
+# compartmentalize imports so that only the relevant packages
+# need to be installed
+#
+try:  # noqa: FURB107
+    from .o2_spharm_vae.o2_spharm_vae import O2SpharmVAE
+except ModuleNotFoundError:
+    pass
+
+try:  # noqa: FURB107
+    from .point_cloud_vae import PointCloudVAE
+except ModuleNotFoundError:
+    pass
