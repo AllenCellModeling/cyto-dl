00a7f7379ac5e26ca62dddd58ff80f55721fc4d9
Guilherme Pires <mail@gpir.es>
2023-03-14 17:55:13 -0700

    fix alternating batch sampler and its test

--- tests/test_alternating_batch_sampler.py
+++ tests/test_alternating_batch_sampler.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import monai
 import numpy as np
 import pandas as pd
@@ -53,21 +55,17 @@ def test_alternating_batch_sampler(shuffle, sampler):
         sampler=sampler,
     )
     index_counts = np.zeros(len(subset))
-    seg_type_counts = {}
+    seg_type_counts = defaultdict(int)
     batches = []
     prev_type = None
 
     for batch in batch_sampler:
-        index_counts[batch] += 1
-        seg_type = [
-            col for col in target_columns if pd.isna(subset.dataset.data.df[col][batch]).sum() == 0
-        ]
+        na_cols = ~subset.dataset.data.df.iloc[batch][target_columns].isna().any()
+        seg_type = na_cols.index[na_cols].tolist()
 
         assert len(seg_type) == 1
-        try:
-            seg_type_counts[seg_type[0]] += 1
-        except KeyError:
-            seg_type_counts[seg_type[0]] = 1
+        seg_type_counts[seg_type[0]] += 1
+
         # alternate types when not shuffling
         if not shuffle and prev_type is not None:
             assert prev_type != seg_type
@@ -77,3 +75,4 @@ def test_alternating_batch_sampler(shuffle, sampler):
     seg_counts = {v for k, v in seg_type_counts.items()}
     assert len(seg_counts) == 1  # all seg types are sampled equally
     assert len(seg_type_counts.keys()) == len(batch_sampler.samplers)  # all seg_types are sampled
+    assert len(batches) == len(batch_sampler)  # yield same number of batches as len
