db9491287b5620fdb45bcef6b0c314524c2f8ef3
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2023-03-17 12:04:04 -0700

    pre-commit

--- scripts/download_test_data.py
+++ scripts/download_test_data.py
@@ -1,30 +1,32 @@
+import shutil
+from pathlib import Path
+
 import boto3
+import fire
+import pandas as pd
 from botocore import UNSIGNED
 from botocore.client import Config
-from pathlib import Path
-import pandas as pd
-import fire
-import shutil
 
-EXAMPLE_DATA_DIR=Path('./data/example_experiment_data')
-EXAMPLE_DATA_FILENAME='s3_paths.csv'
-
+EXAMPLE_DATA_DIR = Path("./data/example_experiment_data")
+EXAMPLE_DATA_FILENAME = "s3_paths.csv"
 
 
 def parse_s3_path(fn):
-    path=Path(fn)
-    assert path.parts[0]=='s3:', f'Expected an s3 path, got {fn}'
-    return  path.parts[1], '/'.join(path.parts[2:]), path.parts[-1]
+    path = Path(fn)
+    assert path.parts[0] == "s3:", f"Expected an s3 path, got {fn}"
+    return path.parts[1], "/".join(path.parts[2:]), path.parts[-1]
+
 
 def setup_paths():
-    for subdir in ['s3_data', 'segmentation', 'labelfree']:
-        (EXAMPLE_DATA_DIR/subdir).mkdir(exist_ok=True, parents=True)
+    for subdir in ("s3_data", "segmentation", "labelfree"):
+        (EXAMPLE_DATA_DIR / subdir).mkdir(exist_ok=True, parents=True)
 
-def download_test_data(limit = -1):
+
+def download_test_data(limit=-1):
     setup_paths()
 
-    s3=boto3.client('s3', config=Config(signature_version=UNSIGNED))
-    df= pd.read_csv(EXAMPLE_DATA_DIR/EXAMPLE_DATA_FILENAME)
+    s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+    df = pd.read_csv(EXAMPLE_DATA_DIR / EXAMPLE_DATA_FILENAME)
     if limit > 0:
         df = df.iloc[:limit]
 
@@ -32,23 +34,24 @@ def download_test_data(limit = -1):
     for col in df.columns:
         for fn in df[col].unique():
             bucket, s3_path, local_file_name = parse_s3_path(fn)
-            local_file_path = EXAMPLE_DATA_DIR/'s3_data'/local_file_name
+            local_file_path = EXAMPLE_DATA_DIR / "s3_data" / local_file_name
             if not local_file_path.exists():
-                s3.download_file(bucket,s3_path, local_file_path)
+                s3.download_file(bucket, s3_path, local_file_path)
             local_data[col].append(local_file_path)
-    
-    local_data = pd.DataFrame(local_data)
-    labelfree_data = local_data[['raw', 'raw']]
-    labelfree_data.columns = ['brightfield', 'signal']    
 
-    for data, data_type in zip([local_data, labelfree_data], ['segmentation', 'labelfree']):
-        for csv_type in ['train', 'test', 'valid']:
-            data.to_csv(EXAMPLE_DATA_DIR/data_type/f"{csv_type}.csv")
+    local_data = pd.DataFrame(local_data)
+    labelfree_data = local_data[["raw", "raw"]]
+    labelfree_data.columns = ["brightfield", "signal"]
+
+    for data, data_type in zip((local_data, labelfree_data), ("segmentation", "labelfree")):
+        for csv_type in ("train", "test", "valid"):
+            data.to_csv(EXAMPLE_DATA_DIR / data_type / f"{csv_type}.csv")
+
 
 def delete_test_data():
-    for subdir in ['segmentation', 'labelfree']:
-        shutil.rmtree(EXAMPLE_DATA_DIR/subdir)
+    for subdir in ("segmentation", "labelfree"):
+        shutil.rmtree(EXAMPLE_DATA_DIR / subdir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fire.Fire(download_test_data)
