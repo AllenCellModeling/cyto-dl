bcebc076ec9595c4a2a8e31629555839a3d98308
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2023-03-21 11:00:05 -0700

    add postprocessing

---
+++ aics_im2im/models/im2im/utils/__init__.py
@@ -0,0 +1,2 @@
+from .omnipose import OmniposeClustering, OmniposeLoss, OmniposePreprocessd
+from .postprocessing import ActThreshLabel, DictToIm, detach
--- aics_im2im/models/im2im/utils/postprocessing.py
+++ aics_im2im/models/im2im/utils/postprocessing.py
@@ -1,12 +1,57 @@
+from typing import Dict
+
 import numpy as np
 import torch
 from skimage.exposure import rescale_intensity
 from skimage.measure import label
 
 
-def rescale(img: torch.Tensor) -> np.ndarray:
-    img = img.detach().cpu().numpy()
-    return rescale_intensity(img, out_range=np.uint8).astype(np.uint8)
+class ActThreshLabel:
+    def __init__(
+        self, activation=torch.nn.Identity(), threshold=None, label=False, dtype=np.uint8, ch=0
+    ):
+        self.activation = activation
+        self.threshold = threshold
+        self.label = label
+        self.dtype = dtype
+        self.ch = ch
+
+    def __call__(self, img: torch.Tensor) -> np.ndarray:
+        img = self.activation(img[self.ch].detach().cpu()).numpy()
+        if self.threshold is not None:
+            img = img > self.threshold
+        if self.label:
+            img = label(img)
+        return img.astype(self.dtype)
+
+
+class Rescale:
+    def __init__(self, output_dtype):
+        self.output_dtype = output_dtype
+
+    def __call__(self, img: torch.Tensor) -> np.ndarray:
+        img = detach(img)
+        return rescale_intensity(img, out_range=self.output_dtype).astype(self.output_dtype)
+
+
+class DictToIm:
+    """Convert dictionary with image values to multichannel image."""
+
+    def __init__(self, keys, allow_missing_keys=False):
+        self.keys = keys
+        self.allow_missing_keys = allow_missing_keys
+
+    def __call__(self, input_dict: Dict[str, torch.Tensor]) -> Dict[str, np.ndarray]:
+        output_img = []
+        for key in self.keys:
+            if key in input_dict:
+                im = detach(input_dict[key]).astype(np.uint8)
+                output_img.append(im)
+            elif not self.allow_missing_keys:
+                raise KeyError(
+                    f"key `{key}` not available. Available keys are {input_dict.keys()}"
+                )
+        return output_img
 
 
 def detach(img: torch.Tensor) -> np.ndarray:
@@ -15,36 +60,3 @@ def detach(img: torch.Tensor) -> np.ndarray:
         img = img.half()
     img = img.numpy()
     return img
-
-
-def sigmoid_rescale(img: torch.Tensor) -> np.ndarray:
-    img = detach(torch.nn.Sigmoid()(img))
-    return (img * 255).astype(np.uint8)
-
-
-def sigmoid_thresh(img: torch.Tensor) -> np.ndarray:
-    img = detach(torch.nn.Sigmoid()(img))
-    return (img > 123).astype(np.uint8)
-
-
-def postprocess_label(img: torch.Tensor) -> np.ndarray:
-    img = detach(torch.nn.Sigmoid()(img))
-    return label(img > 0.5).astype(np.uint16)
-
-
-class max_project:
-    def __init__(self, keys):
-        self.keys = keys
-
-    def __call__(self, input_img: torch.Tensor):
-        for key in self.keys:
-            input_img[key], _ = torch.max(input_img[key].as_tensor(), 1)
-        return input_img
-
-
-def concat_dict(input_dict, keys):
-    output_img = []
-    for key in keys:
-        im = detach(input_dict[key]).astype(np.uint8)
-        output_img.append(im)
-    return np.stack(output_img)
