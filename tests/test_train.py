0654c9f760debba30be1f1b6b23197e2b5b40414
benjijamorris <54606172+benjijamorris@users.noreply.github.com>
2023-03-28 12:20:10 -0700

    remove tests (#165)
    Co-authored-by: Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>

--- tests/test_train.py
+++ tests/test_train.py
@@ -31,45 +31,6 @@ def test_train_fast_dev_run_gpu(cfg_train):
     train(cfg_train)
 
 
-@RunIf(min_gpus=1)
-@pytest.mark.slow
-def test_train_epoch_gpu_amp(cfg_train):
-    """Train 1 epoch on GPU with mixed-precision."""
-    HydraConfig().set_config(cfg_train)
-    with open_dict(cfg_train):
-        cfg_train.trainer.max_epochs = 1
-        cfg_train.trainer.accelerator = "cpu"
-        cfg_train.trainer.precision = 16
-    resolve_readonly(cfg_train)
-    train(cfg_train)
-
-
-@pytest.mark.slow
-def test_train_epoch_double_val_loop(cfg_train):
-    """Train 1 epoch with validation loop twice per epoch."""
-    HydraConfig().set_config(cfg_train)
-    with open_dict(cfg_train):
-        cfg_train.trainer.max_epochs = 1
-        cfg_train.trainer.val_check_interval = 0.5
-
-    resolve_readonly(cfg_train)
-    train(cfg_train)
-
-
-@pytest.mark.slow
-def test_train_ddp_sim(cfg_train):
-    """Simulate DDP (Distributed Data Parallel) on 2 CPU processes."""
-    HydraConfig().set_config(cfg_train)
-    with open_dict(cfg_train):
-        cfg_train.trainer.max_epochs = 2
-        cfg_train.trainer.accelerator = "cpu"
-        cfg_train.trainer.devices = 2
-        cfg_train.trainer.strategy = "ddp_spawn"
-
-    resolve_readonly(cfg_train)
-    train(cfg_train)
-
-
 @pytest.mark.slow
 def test_train_resume(tmp_path, cfg_train):
     """Run 1 epoch, finish, and resume for another epoch."""
