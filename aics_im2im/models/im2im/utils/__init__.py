230bc3ef3c1f972208de293801fec2f90fd68e31
Guilherme Pires <guilherme.pires@alleninstitute.org>
2023-03-29 12:58:17 -0700

    failsafe import omnipose classes (#170)

--- aics_im2im/models/im2im/utils/__init__.py
+++ aics_im2im/models/im2im/utils/__init__.py
@@ -1,2 +1,8 @@
-from .omnipose import OmniposeClustering, OmniposeLoss, OmniposePreprocessd
+try:
+    from .omnipose import OmniposeClustering, OmniposeLoss, OmniposePreprocessd
+except ModuleNotFoundError:
+    OmniposeClustering = None
+    OmniposeLoss = None
+    OmniposePreprocessd = None
+
 from .postprocessing import ActThreshLabel, DictToIm, detach
