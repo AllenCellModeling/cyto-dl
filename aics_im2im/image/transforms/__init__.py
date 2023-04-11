4ff6a060aa7d10df4458bc0841fb39ca22864d03
Benji Morris <benjamin.morris@alleninstitute.org>
2023-04-11 21:21:46 +0000

    fail on import error or module not found

--- aics_im2im/image/transforms/__init__.py
+++ aics_im2im/image/transforms/__init__.py
@@ -5,12 +5,12 @@ from .resize import Resized
 
 try:
     from .o2_mask_transform import O2Mask, O2Maskd
-except ModuleNotFoundError:
+except (ModuleNotFoundError, ImportError):
     O2Mask = None
     O2Maskd = None
 
 try:
     from .so2_random_rotation import SO2RandomRotate, SO2RandomRotated
-except ModuleNotFoundError:
+except (ModuleNotFoundError, ImportError):
     SO2RandomRotate = None
     SO2RandomRotated = None
