892ba488a76764931ac31f4f5b43f7481c2fb81e
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2023-05-12 16:29:31 -0700

    add cropper based on physical pixel size

--- aics_im2im/image/transforms/physical_cropper.py
+++ aics_im2im/image/transforms/physical_cropper.py
@@ -25,21 +25,12 @@ class RandomPhysicalDimsCropper(RandomizableTransform):
         ----------
         keys: Sequence[str]
             list of names dictionary keys to apply transform to
-        x_key: str
-            name of key that is passed into network. Its corresponding scale must be `1`
-        physical_dims: Sequence[int]
-            patch size to sample at resolution 1. Can have len 2 or 3
+        pixel_key: str
+            name of key that contains comma separated list  of physical pixel sizes (e.g. '[0.1,0.1,0.1]')
+        physical_crop_size: Sequence[int]
+            crop size (e.g. in microns)
         patch_per_image: int= 1
             Number of patches to sample per image
-        scales_dict: Dict
-            Dictionary mapping scales key names to their resize factors.
-            For example, `{raw:1, seg: [1.0, 0.5, 0.5]}` would take samples from `raw` of size
-            `physical_dims` and samples from `seg` at `physical_dims`/[1.0,0.5, 0.5]
-        selection_fn: Callable=None
-            Function that takes in an image and returns True or False. Used to
-            decide whether a sampled image should be kept or discarded.
-        max_attempts: int=100
-            max attempts to try sampling patches from an image before quitting
         """
         super().__init__()
         self.keys = keys if isinstance(keys, (list, ListConfig)) else [keys]
@@ -50,30 +41,25 @@ class RandomPhysicalDimsCropper(RandomizableTransform):
         self.allow_missing_keys = allow_missing_keys
 
     @staticmethod
-    def _apply_slice(data, slicee):
-        """If the slice is generated for 3d images, but the passed data is a 2d image (e.g. if we
-        are predicting a 3d image and a 2d image), this will apply the CXY portions of the slice.
-
-        Parameters
-        ----------
-        data:
-            image with shape C[Z]YX
-        slice:
-            slice object with length 3 (generated for 2d images) or 4
-            (generated for 3d images)
-        """
-        return data[tuple(slicee)]
-
-    @staticmethod
     def _generate_slice(start_coords: Sequence[int], roi_size: Sequence[int]) -> slice:
         """Creates slice starting at `start_coords` of size `roi_size`"""
-        return [slice(None, None)] + [
-            slice(start, end) for start, end in zip(start_coords, start_coords + roi_size)
-        ]
+        return tuple(
+            [slice(None, None)]
+            + [slice(start, end) for start, end in zip(start_coords, start_coords + roi_size)]
+        )
+
+    @staticmethod
+    def _parse_pixel_str(pixel_str):
+        try:
+            pix = np.array(pixel_str[1:-1].split(","), dtype="float")
+        except ValueError:
+            raise ValueError('Check that your pixel sizes are of form "[1.0,2.0,3.0]"')
+        return pix
 
     def generate_slices(self, image_dict: Dict, available_keys) -> Dict:
         """Generate dictionary of slices at all scales starting at random point."""
-        roi_size = (self.physical_crop_size / np.asarray(image_dict[self.pixel_key])).astype(int)
+        pixel_dims = self._parse_pixel_str(image_dict[self.pixel_key])
+        roi_size = (self.physical_crop_size / pixel_dims).astype(int)
         max_shape = image_dict[available_keys[0]].shape[-self.spatial_dims :]
         max_start_indices = max_shape - roi_size + 1
         if np.any(max_start_indices < 0):
@@ -91,9 +77,7 @@ class RandomPhysicalDimsCropper(RandomizableTransform):
         patches = []
         for _ in range(self.num_samples):
             slices = self.generate_slices(image_dict, available_keys)
-            patch_dict = {
-                key: self._apply_slice(image_dict[key], slices) for key in available_keys
-            }
+            patch_dict = {key: image_dict[key][slices] for key in available_keys}
             patch_dict.update(meta_dict)
             patches.append(patch_dict)
         return patches
