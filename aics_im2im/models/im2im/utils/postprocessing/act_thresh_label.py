958659e03d94102eac295fbfa3d0139208801638
Benjamin Morris <benjamin.morris@prd-aics-dcp-038.corp.alleninstitute.org>
2023-06-21 13:57:48 -0700

    fix dtype handling

--- aics_im2im/models/im2im/utils/postprocessing/act_thresh_label.py
+++ aics_im2im/models/im2im/utils/postprocessing/act_thresh_label.py
@@ -40,11 +40,14 @@ class ActThreshLabel:
         self.activation = activation
         self.threshold = threshold
         self.label = label
-        self.dtype = dtype
+        self.dtype = self._get_dtype(dtype)
         self.ch = ch
-        self.rescale_dtype = (
-            get_class(rescale_dtype) if rescale_dtype is not None else rescale_dtype
-        )
+        self.rescale_dtype = self._get_dtype(rescale_dtype)
+        if self.rescale_dtype is not None:
+            self.dtype = self.rescale_dtype
+
+    def _get_dtype(self, dtype: DTypeLike) -> DTypeLike:
+        return get_class(dtype) if dtype is not None else dtype
 
     def __call__(self, img: torch.Tensor) -> np.ndarray:
         img = self.activation(img[self.ch].detach().cpu().float()).numpy()
