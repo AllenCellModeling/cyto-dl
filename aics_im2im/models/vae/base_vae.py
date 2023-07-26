ad3fcb7c891747d7ddb3648e4fbf04a0f7b65901
Guilherme Pires <mail@gpir.es>
2023-07-26 13:39:15 -0700

    simplify base vae elbo

--- aics_im2im/models/vae/base_vae.py
+++ aics_im2im/models/vae/base_vae.py
@@ -156,13 +156,13 @@ class BaseVAE(BaseModel):
                 rcl_reduced[key] = rcl_per_input_dimension[key]
 
         kld_per_part = {
-            part: prior(z[part], mode="kl", reduction="none") for part, prior in self.prior.items()
+            part: prior(z[part], mode="kl", reduction="none").sum(dim=-1).mean()
+            for part, prior in self.prior.items()
         }
 
-        kld_per_part_summed = {part: kl.sum(dim=-1).mean() for part, kl in kld_per_part.items()}
-
-        total_kld = sum(kld_per_part_summed.values())
+        total_kld = sum(kld_per_part.values())
         total_recon = sum(rcl_reduced.values())
+
         return (
             total_recon + self.beta * total_kld,
             total_recon,
