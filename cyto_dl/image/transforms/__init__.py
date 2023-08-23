ca5b7e5218f23afb51edd857d01bfdeea6e612f3
Ritvik Vasan <ritvik.vasan@prd-aics-dcp-038.corp.alleninstitute.org>
2023-08-23 16:26:05 -0700

    update pcloud requirements and add skimage reader + image configs

--- cyto_dl/image/transforms/__init__.py
+++ cyto_dl/image/transforms/__init__.py
@@ -3,7 +3,8 @@ from .multiscale_cropper import RandomMultiScaleCropd
 from .project import MaxProjectd
 from .resize import Resized
 from .save import Save, Saved
-
+from .clip import Clip, Clipd
+from .contrastadjust import ContrastAdjust, ContrastAdjustd
 try:
     from .rotation_mask_transform import RotationMask, RotationMaskd
 except (ModuleNotFoundError, ImportError):
