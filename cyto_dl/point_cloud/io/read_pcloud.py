fe92b77b41b0df72cee9a71cd9838089b194acd1
Ritvik Vasan <ritvik.vasan@prd-aics-dcp-038.corp.alleninstitute.org>
2023-09-05 12:58:31 -0700

    fix codespell error

--- cyto_dl/point_cloud/io/read_pcloud.py
+++ cyto_dl/point_cloud/io/read_pcloud.py
@@ -35,7 +35,7 @@ class ReadPointCloud(MapTransform):
         scale: float
             scale factor for X,Y,Z coordinates - e.g. X' = scale * X
         num_cols: int
-            Number of colums to sample from the saved point cloud
+            Number of columns to sample from the saved point cloud
             This is relevant for ply files saved with additional scalar features
             here we assume first 3 columns are X, Y, Z coordinates
         norm: bool
