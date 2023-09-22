2cdc1f1b76ae735c8a172667fde69b6662e59ce1
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2023-09-22 15:41:42 -0700

    add non-dict resize

--- cyto_dl/image/transforms/resize.py
+++ cyto_dl/image/transforms/resize.py
@@ -6,6 +6,68 @@ from monai.data.meta_tensor import MetaTensor
 from monai.transforms import Transform
 
 
+class Resize(Transform):
+    """Transform to resize image by`scale_factor`"""
+
+    def __init__(
+        self,
+        scale_factor: Union[float, Sequence[float]],
+        spatial_dims: int = 3,
+        mode: str = "nearest-exact",
+        align_corners: Union[bool, None] = None,
+        recompute_scale_factor: bool = False,
+        antialias: bool = False,
+    ):
+        """
+        Parameters
+        ----------
+        scale_factor: int
+            output size will be `img.shape*scale_factor`
+        spatial_dims: int
+            whether inputs are 2d or 3d
+        mode:
+            interpolation method. For more details see:
+            https://pytorch.org/docs/stable/generated/torch.nn.functional.interpolate.html # noqa
+        align_corners:
+            see https://pytorch.org/docs/stable/generated/torch.nn.functional.interpolate.html # noqa
+        recompute_scale_factor:
+            see https://pytorch.org/docs/stable/generated/torch.nn.functional.interpolate.html # noqa
+        """
+        super().__init__()
+        assert spatial_dims in (2, 3), f"Patch must be 2D or 3D, got {spatial_dims}"
+        self.scale_factor = np.asarray(scale_factor)
+        assert self.scale_factor.size in (
+            1,
+            spatial_dims,
+        ), f"Scale factor must have length 1 or {spatial_dims}, got {len(self.scale_factor)}"
+        if self.scale_factor.size == 1:
+            self.scale_factor = np.tile(self.scale_factor, spatial_dims)
+        self.spatial_dims = spatial_dims
+        self.mode = mode
+        self.align_corners = align_corners
+        self.recompute_scale_factor = recompute_scale_factor
+        self.antialias = antialias
+
+    def __call__(self, img):
+        out_size = list(
+            map(
+                round,
+                np.asarray(img.shape[-self.spatial_dims :]) * self.scale_factor,
+            )
+        )
+        if len(img.shape) != self.spatial_dims + 1:
+            raise ValueError("Images must have CZYX or CYX dimensions")
+        img = img.as_tensor() if isinstance(img, MetaTensor) else img
+
+        return torch.nn.functional.interpolate(
+            input=img.unsqueeze(0),
+            size=out_size,
+            mode=self.mode,
+            align_corners=self.align_corners,
+            antialias=self.antialias,
+        ).squeeze(0)
+
+
 class Resized(Transform):
     """Transform to resize image by`scale_factor`"""
 
@@ -55,28 +117,14 @@ class Resized(Transform):
         self.recompute_scale_factor = recompute_scale_factor
         self.antialias = antialias
         self.allow_missing_keys = allow_missing_keys
+        self.resizer = Resize(
+            scale_factor, spatial_dims, mode, align_corners, recompute_scale_factor, antialias
+        )
 
     def __call__(self, img):
         for key in self.keys:
             if key in img.keys():
-                out_size = list(
-                    map(
-                        round,
-                        np.asarray(img[key].shape[-self.spatial_dims :]) * self.scale_factor,
-                    )
-                )
-                raw_img = img[key]
-                if len(raw_img.shape) != self.spatial_dims + 1:
-                    raise ValueError("Images must have CZYX or CYX dimensions")
-                raw_img = raw_img.as_tensor() if isinstance(raw_img, MetaTensor) else raw_img
-
-                img[key] = torch.nn.functional.interpolate(
-                    input=raw_img.unsqueeze(0),
-                    size=out_size,
-                    mode=self.mode,
-                    align_corners=self.align_corners,
-                    antialias=self.antialias,
-                ).squeeze(0)
+                img[key] = self.resizer(img[key])
             elif not self.allow_missing_keys:
                 raise KeyError(f"Key {key} not found in data. Available keys are {img.keys()}")
         return img
