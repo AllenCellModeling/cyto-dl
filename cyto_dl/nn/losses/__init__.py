30e0a310ed0df8e0458821e5741d41c9d22f3efb
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2023-10-19 15:51:55 -0700

    fix geomloss import

--- cyto_dl/nn/losses/__init__.py
+++ cyto_dl/nn/losses/__init__.py
@@ -4,7 +4,6 @@ from .continuous_bernoulli import CBLogLoss
 from .cosine_loss import CosineLoss
 from .gan_loss import GANLoss, Pix2PixHD
 from .gaussian_nll_loss import GaussianNLLLoss
-from .geomloss import GeomLoss
 from .threshold_loss import ThresholdLoss
 from .weibull import WeibullLogLoss
 from .weighted_mse_loss import WeightedMSELoss
@@ -13,3 +12,8 @@ try:
     from .spharm_loss import SpharmLoss
 except (ModuleNotFoundError, ImportError):
     SpharmLoss = None
+
+try:
+    from .geomloss import GeomLoss
+except (ModuleNotFoundError, ImportError):
+    GeomLoss = None
