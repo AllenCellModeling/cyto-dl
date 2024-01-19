1c0ffb5c61f285c886969f82fdbcab8cb3cceec1
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2024-01-19 14:12:05 -0800

    add delete test data

--- scripts/download_test_data.py
+++ scripts/download_test_data.py
@@ -1,6 +1,6 @@
-from cyto_dl.utils.download_test_data import download_test_data
 import fire
 
+from cyto_dl.utils.download_test_data import delete_test_data, download_test_data
 
 if __name__ == "__main__":
     fire.Fire(download_test_data)
