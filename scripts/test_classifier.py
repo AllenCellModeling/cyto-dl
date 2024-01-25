2cea8f89f41f4fb2aad7f1bf34ad5f5c08a2d03c
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2024-01-25 15:43:44 -0800

    remove old py files

--- scripts/test_classifier.py
+++
@@ -1,4 +0,0 @@
-
-from cyto_dl.datamodules.track_classification import TrackClassificationDatamodule
-d = TrackClassificationDatamodule(csv_path='//allen/aics/assay-dev/users/Benji/CurrentProjects/breakdown_classifier_cyto_dl/data/', augmentations='')
-d.prepare_data()
