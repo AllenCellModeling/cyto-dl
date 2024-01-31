3974dc2cd0900d6535adaa8a0d3d706fac786ffa
benjijamorris <54606172+benjijamorris@users.noreply.github.com>
2024-01-31 13:03:30 -0800

    Feature/breakdown classifier (#329)
    * add all
    
    * remove unused code
    
    * clean up transforms
    
    * remove unused configs
    
    * deletemore configs
    
    * remove old py files
    
    * remove old datamodule
    
    * precommit and updatedocs
    
    * add initial configs
    
    * update with feedback from Ritvik
    
    * precommit
    
    ---------
    
    Co-authored-by: Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
    Co-authored-by: Benjamin Morris <benjamin.morris@prd-aics-dcp-036.corp.alleninstitute.org>

---
+++ cyto_dl/image/transforms/track_transforms.py
@@ -0,0 +1,181 @@
+import copy
+from typing import List, Union
+
+import numpy as np
+import torch
+from monai.transforms import RandomizableTransform, Resize, Transform
+from omegaconf import ListConfig
+
+
+class GenerateTrackLabels(Transform):
+    """Transform to generate track labels from breakdown and formation labels."""
+
+    def __init__(
+        self,
+        img_key: str = "img",
+        formation_key: str = "formation",
+        breakdown_key: str = "breakdown",
+        track_start_key: str = "track_start",
+        label_key: str = "label",
+    ):
+        """
+        Parameters
+        ----------
+        img_key: str
+            Key with image
+        formation_key: str
+            Key with formation
+        breakdown_key: str
+            Key with breakdown
+        track_start_key: str
+            Key with track start
+        label_key: str
+            Key to save label into
+        """
+        super().__init__()
+        self.img_key = img_key
+        self.formation_key = formation_key
+        self.breakdown_key = breakdown_key
+        self.track_start_key = track_start_key
+        self.label_key = label_key
+
+    def __call__(self, img_dict):
+        n_timepoints = img_dict[self.img_key].shape[0]
+        formation_idx = int(img_dict[self.formation_key] - img_dict[self.track_start_key])
+        breakdown_idx = int(img_dict[self.breakdown_key] - img_dict[self.track_start_key])
+
+        # 0: normal, 1: mitotic
+        tp_labels = np.zeros(n_timepoints)
+        if 0 <= formation_idx < len(tp_labels):
+            tp_labels[:formation_idx] = 1
+
+        if 0 <= breakdown_idx < len(tp_labels):
+            tp_labels[breakdown_idx + 1 :] = 1
+        img_dict[self.label_key] = tp_labels
+        return img_dict
+
+
+class TrackCrop(RandomizableTransform):
+    """Transform to randomly crop track to first n or last n timepoints."""
+
+    def __init__(
+        self,
+        img_key: str = "img",
+        label_key: str = "label",
+        p_first: float = 0.15,
+        p_last: float = 0.1,
+        percentage=[0.2, 0.7],
+        max_crop_length: int = 20,
+    ):
+        """
+        Parameters
+        ----------
+        img_key: str
+            Key with image
+        p_first: float
+            Probability of cropping to start of track
+        p_last: float
+            Probability of cropping to end of track
+        percentage: list
+            Range of percentages of track length to crop
+        max_crop_length: int
+            Maximum number of timepoints to crop
+        """
+        super().__init__()
+        self.img_key = img_key
+        self.label_key = label_key
+        self.p_first = p_first
+        self.p_last = p_last
+        self.percentage = np.array(percentage)
+        self.max_crop_length = max_crop_length
+
+    def __call__(self, img_dict):
+        new_im_dict = copy.deepcopy(img_dict)
+        new_im = img_dict[self.img_key]
+
+        # randomly select number of timepoints between percentage range of track length
+        n = self.R.randint(*(self.percentage * new_im.shape[0]).astype(int), size=1)[0]
+        n = min(self.max_crop_length, n)
+        # crop to start of track
+        if self.R.random() < self.p_first:
+            new_im_dict[self.img_key] = new_im[:n]
+            new_im_dict[self.label_key] = new_im_dict[self.label_key][:n]
+        # crop to end of track
+        elif self.R.random() < self.p_last:
+            new_im_dict[self.img_key] = new_im[-n:]
+            new_im_dict[self.label_key] = new_im_dict[self.label_key][-n:]
+        return new_im_dict
+
+
+class PerChannel(Transform):
+    """Transform to apply same transform to each channel of image."""
+
+    def __init__(
+        self,
+        keys: Union[str, List, ListConfig],
+        transform: Transform,
+        allow_missing_keys: bool = False,
+    ):
+        """
+        Parameters
+        ----------
+        keys: list
+            List of keys to apply transform to
+        transform: Transform
+            Transform to apply to each channel
+        allow_missing_keys: bool
+            Whether to allow missing keys
+        """
+        super().__init__()
+        self.transform = transform
+        self.keys = keys if isinstance(keys, (list, ListConfig)) else [keys]
+        self.allow_missing_keys = allow_missing_keys
+
+    def __call__(self, img_dict):
+        new_im_dict = copy.deepcopy(img_dict)
+        for key in self.keys:
+            if key not in new_im_dict and not self.allow_missing_keys:
+                raise KeyError(
+                    f"Key {key} not found in image dictionary. Available keys are {list(new_im_dict.keys())}"
+                )
+            for i in range(new_im_dict[key].shape[0]):
+                new_im_dict[key][i] = self.transform(new_im_dict[key][i])
+        return new_im_dict
+
+
+class CropResize(RandomizableTransform):
+    def __init__(
+        self, keys: Union[str, List, ListConfig], max_shift=8, allow_missing_keys: bool = False
+    ):
+        """
+        Parameters
+        ----------
+        keys: list
+            List of keys to apply transform to
+        max_shift: int
+            Maximum number of pixels to shift image by before resizing
+        allow_missing_keys: bool
+            Whether to allow missing keys
+        """
+        super().__init__()
+        self.keys = keys if isinstance(keys, (list, ListConfig)) else [keys]
+        self.max_shift = max_shift
+        self.allow_missing_keys = allow_missing_keys
+
+    def __call__(self, img_dict):
+        new_im_dict = copy.deepcopy(img_dict)
+
+        for key in self.keys:
+            if key not in new_im_dict and not self.allow_missing_keys:
+                raise KeyError(
+                    f"Key {key} not found in image dictionary. Available keys are {list(new_im_dict.keys())}"
+                )
+            resizer = Resize(new_im_dict[key].shape[-2:])
+            resized_movie = []
+            for im in new_im_dict[key]:
+                shift = self.R.randint(0, self.max_shift, size=4)
+                im = im[shift[0] : im.shape[0] - shift[1], shift[2] : im.shape[1] - shift[3]]
+                im = resizer(im.unsqueeze(0)).squeeze(0)
+                resized_movie.append(im)
+            new_im_dict[key] = torch.stack(resized_movie)
+        return new_im_dict
---
+++ cyto_dl/models/classification_transformer.py
@@ -0,0 +1,172 @@
+import sys
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from aicsimageio.writers import OmeTiffWriter
+from matplotlib import pyplot as plt
+from PIL import Image, ImageDraw, ImageFont
+from skimage.exposure import rescale_intensity
+from torchmetrics import MeanMetric
+from torchmetrics.classification import MulticlassF1Score
+
+from cyto_dl.models.base_model import BaseModel
+from cyto_dl.models.utils import find_indices
+
+
+class Classifier(BaseModel):
+    def __init__(
+        self,
+        *,
+        model: nn.Module,
+        x_key: str,
+        num_classes: int,
+        y_key: str = "label",
+        save_dir="./",
+        save_movie: bool = True,
+        save_images_every_n_epochs=10,
+        compile=False,
+        **base_kwargs,
+    ):
+        """
+        Parameters
+        ----------
+        model: nn.Module
+            model network, parameters are shared between task heads
+        x_key: str
+            key of input image in batch
+        save_dir="./"
+            directory to save images during training and validation
+        save_images_every_n_epochs=1
+            Frequency to save out images during training
+        compile: False
+            Whether to compile the model using torch.compile
+        **base_kwargs:
+            Additional arguments passed to BaseModel
+        """
+        _DEFAULT_METRICS = {
+            "train/loss": MeanMetric(),
+            "val/loss": MeanMetric(),
+            "test/loss": MeanMetric(),
+            "train/f1": MulticlassF1Score(num_classes=num_classes),
+            "val/f1": MulticlassF1Score(num_classes=num_classes),
+        }
+        metrics = base_kwargs.pop("metrics", _DEFAULT_METRICS)
+        super().__init__(metrics=metrics, **base_kwargs)
+
+        self.automatic_optimization = True
+        for stage in ("train", "val", "test", "predict"):
+            (Path(save_dir) / f"{stage}_images").mkdir(exist_ok=True, parents=True)
+        (Path(save_dir) / "plots").mkdir(exist_ok=True, parents=True)
+        if compile is True and not sys.platform.startswith("win"):
+            self.model = torch.compile(model)
+        else:
+            self.model = model
+
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def should_save_image(self, batch_idx, stage):
+        return (
+            stage in ("test", "predict")
+            or (self.current_epoch + 1) % self.hparams.save_images_every_n_epochs == batch_idx == 0
+        )
+
+    def save_images(self, batch, stage, logits, name=None):
+        pred = torch.argmax(logits, dim=1)
+
+        if stage == "predict":
+            label = torch.zeros_like(pred)
+        else:
+            label = batch[self.hparams.y_key].squeeze(0)
+        # save labeled movie of prediction
+
+        font = ImageFont.load_default()
+        # Define text positions
+        movie = []
+        for p, l, frame in zip(pred, label, batch[self.hparams.x_key].cpu().numpy().squeeze()):
+            # Convert the NumPy array to a PIL Image
+            image = Image.fromarray(frame.astype(float))
+
+            # Create a drawing context
+            draw = ImageDraw.Draw(image)
+            # Draw text on the image
+            draw.text((0, 0), f"P:{p}", fill=2 * frame.max(), font=font)
+            if stage != "predict":
+                draw.text((0, 20), f"L:{int(l)}", fill=2 * frame.max(), font=font)
+
+            # Convert the image back to a NumPy array
+            movie.append(np.array(image))
+
+        if name is None:
+            name = self.current_epoch
+
+        movie = rescale_intensity(np.stack(movie), out_range=(0, 255)).astype(np.uint8)
+
+        OmeTiffWriter.save(
+            uri=Path(self.hparams.save_dir) / f"{stage}_images" / f"{name}.ome.tiff",
+            data=movie,
+        )
+
+    def model_step(self, stage, batch, batch_idx):
+        assert batch[self.hparams.x_key].shape[0] == 1, "batch size must be 1"
+        logits = self(batch[self.hparams.x_key]).squeeze(0)
+        labels = batch[self.hparams.y_key].squeeze(0)
+        if self.should_save_image(batch_idx, stage):
+            self.save_images(batch, stage, logits)
+        loss = self.loss_fn(logits, labels.long())
+        return loss, logits.argmax(dim=1), labels
+
+    def predict_step(self, batch, batch_idx):
+        assert batch[self.hparams.x_key].shape[0] == 1, "batch size must be 1"
+        logits = self(batch[self.hparams.x_key]).squeeze(0)
+        if self.hparams.save_movie:
+            self.save_images(
+                batch,
+                "predict",
+                logits,
+                name=f"{batch['track_id'].cpu().item()}",
+            )
+
+        preds = torch.argmax(logits, dim=1).cpu().numpy()
+        track_midpoint = preds.shape[0] // 2
+        track_start = batch["track_start"].cpu().item()
+        track_id = batch["track_id"].cpu().item()
+
+        # breakdowns are transitions from interphase (0) to mitotic (1)
+        breakdowns = find_indices(preds, [0, 1])
+        # formations are transitions from mitotic (1) to interphase (0)
+        formations = find_indices(preds, [1, 0])
+
+        # -1 -> no formation/breakdown
+        if formations.size == 0:
+            formation = -1
+        else:
+            # when multiple formations present, take first
+            formation = np.min(formations)
+            # formation should occur in the first half of the track
+            formation = formation if formation > track_midpoint else -1
+
+        if breakdowns.size == 0:
+            breakdown = -1
+        else:
+            # when multiple breakdowns present, take last
+            breakdown = np.max(breakdowns)
+            # breakdown should occur in the second half of the track
+            breakdown = breakdown if breakdown > track_midpoint else -1
+
+        predictions = {
+            "track_id": track_id,
+            "formation": formation + track_start if formation >= 0 else -1,
+            "breakdown": breakdown + track_start if breakdown >= 0 else -1,
+        }
+
+        pd.DataFrame(predictions).to_csv(
+            Path(self.hparams.save_dir) / f"predictions_batch={batch_idx}.csv", index=False
+        )
+
+        return predictions
