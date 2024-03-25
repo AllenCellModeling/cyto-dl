45b1a5a877407868e1a388a86a683e08cf5c256d
Daniel Saelid <daniel.saelid@alleninstitute.org>
2024-03-25 16:58:47 -0700

    added lots of config methods, WIP

--- cyto_dl/api/config/cyto_dl_config.py
+++ cyto_dl/api/config/cyto_dl_config.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Optional, Dict, Any
+from typing import Optional, List, Any
+from hydra import compose, initialize_config_dir
+from hydra.core.global_hydra import GlobalHydra
+from omegaconf import OmegaConf, open_dict, DictConfig
+import pyrootutils
+from copy import deepcopy
 from cyto_dl.api.data import *
 
 
@@ -9,9 +14,6 @@ class CytoDLConfig(ABC):
     A CytoDLConfig represents the configuration for a CytoDLModel. Can be
     passed to CytoDLModel on initialization to create a fully-functional model.
     """
-    # Note: internally, Config classes will just be used to store overrides and
-    # possibly a config filepath, while CytoDLModel will handle actually generating
-    # the full config
     @abstractmethod
     def __init__(self, config_filepath: Optional[Path]=None, train: bool=True):
         """
@@ -20,26 +22,96 @@ class CytoDLConfig(ABC):
         :param train: indicates whether this config will be used for training or prediction
         """
         self._config_filepath: str = config_filepath
-        self._overrides: Dict[str, Any] = {}
-        self._overrides["train"] = train
-        self._overrides["test"] = train
+        self._train: bool = train
+        self._cfg: DictConfig = OmegaConf.load(config_filepath) if config_filepath else self._generate_default_config()
+        OmegaConf.update(self._cfg, "train", train)
+        OmegaConf.update(self._cfg, "test", train)
+        # afaik, task_name isn't used outside of template_utils.py - do we need to support this?
+        OmegaConf.update(self._cfg, "task_name", "train" if train else "predict")
+
+        # we currently have an override for ['mode'] in ml-seg, but I can't find top-level 'mode' in the configs, 
+        # do we need to support this?
     
+    def _generate_default_config(self) -> DictConfig:
+        cfg_dir: Path = pyrootutils.find_root(search_from=__file__, indicator=("pyproject.toml", "README.md")) / "configs"
+        GlobalHydra.instance().clear()
+        with initialize_config_dir(version_base="1.2", config_dir=str(cfg_dir)):
+            cfg: DictConfig = compose(
+                config_name="train.yaml" if self._train else "eval.yaml",
+                return_hydra_config=True,
+                overrides=[f"experiment=im2im/{self._get_experiment_type().name}"],
+            )
+        with open_dict(cfg):
+            del cfg["hydra"]
+            cfg.extras.enforce_tags = False
+            cfg.extras.print_config = False
+        return cfg
+
     @abstractmethod
-    def get_experiment_type(self) -> ExperimentType:
+    def _get_experiment_type(self) -> ExperimentType:
         """
         Return experiment type for this config (e.g. segmentation_plugin, gan, etc)
         """
         pass
-
-    def get_config_filepath(self) -> Optional[Path]:
-        return self._config_filepath
     
-    def set_hardware_type(self, hardware_type: HardwareType) -> None:
-        self._overrides["trainer.accelerator"] = hardware_type.value
+    def _key_exists(self, k: str) -> bool:
+        keys: List[str] = k.split(".")
+        curr_dict: DictConfig = self._cfg
+        while keys:
+            key: str = keys.pop(0)
+            if not key in curr_dict:
+                return False
+            curr_dict = curr_dict[key]
+        return True
+    
+    def _set_cfg(self, k: str, v: Any) -> None:
+        if not self._key_exists(k):
+            raise KeyError(f"{k} not found in config dict")
+        OmegaConf.update(self._cfg, k, v)
+
+    def _get_cfg(self, k: str) -> Any:
+        if not self._key_exists(k):
+            raise KeyError(f"{k} not found in config dict")
+        return OmegaConf.select(self._cfg, k)
     
     def set_experiment_name(self, name: str) -> None:
-        self._overrides["experiment_name"] = name
+        self._set_cfg("experiment_name", name)
     
+    def get_experiment_name(self) -> str:
+        return self._get_cfg("experiment_name")
+
     def set_ckpt_path(self, ckpt_path: Path) -> None:
-        self._overrides["ckpt_path"] = str(ckpt_path.resolve())
+        self._set_cfg("ckpt_path", str(ckpt_path.resolve()))
+    
+    def get_ckpt_path(self) -> Path:
+        return Path(self._get_cfg("ckpt_path"))
+
+    def set_hardware_type(self, hardware_type: HardwareType) -> None:
+        self._set_cfg("trainer.accelerator", hardware_type.value)
+    
+    def get_hardware_type(self) -> HardwareType:
+        return HardwareType(self._get_cfg("trainer.accelerator"))
+
+    def set_max_epochs(self, max_epochs: int) -> None:
+        self._set_cfg("trainer.max_epochs", max_epochs)
+    
+    def get_max_epochs(self) -> int:
+        return self._get_cfg("trainer.max_epochs")
+    
+    def set_output_dir(self, output_dir: Path) -> None:
+        self._set_cfg("paths.output_dir", str(output_dir))
+    
+    def get_output_dir(self) -> Path:
+        return Path(self._get_cfg("paths.output_dir"))
+    
+    # I can't find where this is actually used in cyto_dl, do we need to support this?
+    def set_work_dir(self, work_dir: Path) -> None:
+        self._set_cfg("paths.work_dir", str(work_dir))
+    
+    def get_work_dir(self) -> Path:
+        return Path(self._get_cfg("paths.work_dir"))
+    
+    def get_config(self) -> DictConfig:
+        return deepcopy(self._cfg)
+
 
--- cyto_dl/api/config/segmentation_plugin_config.py
+++ cyto_dl/api/config/segmentation_plugin_config.py
@@ -1,11 +1,62 @@
-from typing import Optional
+from typing import Optional, Tuple, List
 from pathlib import Path
 from cyto_dl.api.config import CytoDLConfig
 from cyto_dl.api.data import *
-
+# TODO: get rid of using Path or any non-primitive data type since there are format strings in config sometimes...
 class SegmentationPluginConfig(CytoDLConfig):
-    def __init__(self, config_filepath: Optional[Path]=None):
-        super().__init__(config_filepath)
+    def __init__(self, config_filepath: Optional[Path] = None, train: bool = True):
+        super().__init__(config_filepath, train)
     
-    def get_experiment_type(self) -> ExperimentType:
+    def _get_experiment_type(self) -> ExperimentType:
         return ExperimentType.SEGMENTATION_PLUGIN
+    
+    # a lot of these keys are duplicated across the im2im experiment types (but not present in top-level
+    # train.yaml or eval.yaml) - should we move these into the top-level configs and move these setters and
+    # getters accordingly? 
+    def set_spatial_dims(self, spatial_dims: int) -> None:
+        self._set_cfg("spatial_dims", spatial_dims)
+    
+    def get_spatial_dims(self) -> int:
+        return self._get_cfg("spatial_dims")
+    
+    def set_input_channel(self, input_channel: int) -> None:
+        self._set_cfg("input_channel", input_channel)
+    
+    def get_input_channel(self) -> int:
+        return self._get_cfg("input_channel")
+    
+    def set_raw_image_channels(self, channels: int) -> None:
+        self._set_cfg("raw_im_channels", channels)
+    
+    def get_raw_image_channels(self) -> int:
+        return self._get_cfg("raw_im_channels")
+    
+    def set_data_path(self, data_path: Path) -> None:
+        self._set_cfg("data.path", str(data_path))
+    
+    def get_data_path(self) -> Path:
+        return Path(self._get_cfg("data.path"))
+    
+    # TODO: is there a better way to deal with column names + split columns?
+    def set_manifest_column_names(self, source: str, target1: str, target2: str, merge_mask: str, exclude_mask: str, base_image: str) -> None:
+        self._set_cfg("source_col", source)
+        self._set_cfg("target_col1", target1)
+        self._set_cfg("target_col2", target2)
+        self._set_cfg("merge_mask_col", merge_mask)
+        self._set_cfg("exclude_mask_col", exclude_mask)
+        self._set_cfg("base_image_col", base_image)
+    
+    def get_manifest_column_names(self) -> Tuple[str, str, str, str, str, str]:
+        return self._get_cfg("source_col"), self._get_cfg("target_col1"), self._get_cfg("target_col2"), self._get_cfg("merge_mask_col"), self._get_cfg("exclude_mask_col"), self._get_cfg("base_image_col")
+    
+    def set_split_column(self, split_column: str) -> None:
+        self._set_cfg("data.split_column", split_column)
+        # need to also add it to the list of columns
+        existing_cols: List[str] = self._get_cfg("data.columns")
+        if len(existing_cols) > len(self.get_manifest_column_names()):
+            existing_cols[-1] = split_column
+        else:
+            existing_cols.append(split_column)
+    
+    def get_split_column(self) -> str:
+        self._get_cfg("data.split_column")
