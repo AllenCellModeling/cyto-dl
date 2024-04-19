e2e80d0e0dab5854d2f94884d30699055cdf2d10
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-04-19 12:24:37 -0700

    fix mask head

--- cyto_dl/nn/head/__init__.py
+++ cyto_dl/nn/head/__init__.py
@@ -1,4 +1,5 @@
 from .base_head import BaseHead
 from .gan_head import GANHead
 from .gan_head_superres import GANHead_resize
+from .mask_head import MaskHead
 from .res_blocks_head import ResBlocksHead
