c539c7e90c3fc46c1cdb561bbbf3485a874ccb31
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-05-20 09:50:28 -0700

    allow multi-ch reading from comma separatedlist

--- cyto_dl/image/io/aicsimage_loader.py
+++ cyto_dl/image/io/aicsimage_loader.py
@@ -49,6 +49,12 @@ class AICSImageLoaderd(Transform):
         self.dtype = dtype
         self.dask_load = dask_load
 
+    def split_args(self, arg):
+        if ',' in str(arg):
+            return list(map(int, arg.split(",")))
+        else:
+            return arg
+
     def __call__(self, data):
         # copying prevents the dataset from being modified inplace - important when using partially cached datasets so that the memory use doesn't increase over time
         data = data.copy()
@@ -58,7 +64,7 @@ class AICSImageLoaderd(Transform):
         img = BioImage(path)
         if self.scene_key in data:
             img.set_scene(data[self.scene_key])
-        kwargs = {k: data[k] for k in self.kwargs_keys}
+        kwargs = {k: self.split_args(data[k]) for k in self.kwargs_keys if k in data}
         if self.dask_load:
             img = img.get_image_dask_data(**kwargs).compute()
         else:
