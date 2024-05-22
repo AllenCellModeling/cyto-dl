50646aec759da0bf300852f9a1d1afe68f68f7e1
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-05-22 13:59:56 -0700

    start of mask2former

--- cyto_dl/nn/vits/hiera_mae.py
+++ cyto_dl/nn/vits/hiera_mae.py
@@ -58,6 +58,7 @@ class HieraEncoder(torch.nn.Module):
         patch_size: List[int] = (16, 16, 16),
         mask_ratio: Optional[float] = 0.75,
         context_pixels: Optional[List[int]] = [0, 0, 0],
+        save_layers: Optional[bool] = True,
     ) -> None:
         """
         Parameters
@@ -82,6 +83,7 @@ class HieraEncoder(torch.nn.Module):
             Whether to output linear combination of intermediate layers as final output like CrossMAE
         """
         super().__init__()
+        self.save_layers=  save_layers
         # TODO decide how to deal with class token. Do we add it as an extra patch token per mask unit? Leaving out for now...
         self.patchify = PatchifyHiera(
             patch_size, num_patches, mask_ratio, num_mask_units, emb_dim, spatial_dims, context_pixels
@@ -141,17 +143,20 @@ class HieraEncoder(torch.nn.Module):
 
         # mask unit attention
         mask_unit_embeddings = 0.0
+        save_layers = []
         for i, block in enumerate(self.mask_unit_transformer):
             patches = block(patches)
             if i in self.save_block_idxs:
                 mask_unit_embeddings += self.spatial_mergers[f'block_{i}'](patches)
+                if self.save_layers:
+                    save_layers.append(patches)
 
         # combine mask units and tokens for full self attention transformer
         mask_unit_embeddings = rearrange(mask_unit_embeddings, "b n_mu t c -> b (n_mu t) c")
         mask_unit_embeddings = self.self_attention_transformer(mask_unit_embeddings)
         mask_unit_embeddings= self.layer_norm(mask_unit_embeddings)
 
-        return mask_unit_embeddings, mask, forward_indexes, backward_indexes
+        return mask_unit_embeddings, mask, forward_indexes, backward_indexes, save_layers
 
 
 class HieraMAE(torch.nn.Module):
@@ -218,7 +223,7 @@ class HieraMAE(torch.nn.Module):
         )
 
     def forward(self, img):
-        features, mask, forward_indexes, backward_indexes = self.encoder(img)
+        features, mask, forward_indexes, backward_indexes, save_layers = self.encoder(img)
         features = rearrange(features, "b t c -> t b c")
         predicted_img = self.decoder(features, forward_indexes, backward_indexes)
         return predicted_img, mask
@@ -264,17 +269,18 @@ class HieraSeg(torch.nn.Module):
             patch_size=patch_size,
             mask_ratio=0,
             context_pixels=context_pixels,
+            save_layers=True
         )
-        model = torch.load(encoder_ckpt, map_location="cuda:0")
-        enc_state_dict = {
-            k.replace("backbone.encoder.", ""): v
-            for k, v in model["state_dict"].items()
-            if "encoder" in k and "intermediate" not in k
-        }
-        self.encoder.load_state_dict(enc_state_dict, strict=False)
-        for name, param in self.encoder.named_parameters():
-            # allow different weighting of internal activations for finetuning
-            param.requires_grad = False
+        # model = torch.load(encoder_ckpt, map_location="cuda:0")
+        # enc_state_dict = {
+        #     k.replace("backbone.encoder.", ""): v
+        #     for k, v in model["state_dict"].items()
+        #     if "encoder" in k and "intermediate" not in k
+        # }
+        # self.encoder.load_state_dict(enc_state_dict, strict=False)
+        # for name, param in self.encoder.named_parameters():
+        #     # allow different weighting of internal activations for finetuning
+        #     param.requires_grad = False
 
         # "patches" to the decoder are actually mask units, so num_patches is num_mask_units, patch_size is mask unit size
         mask_unit_size = ((np.array(num_patches) * np.array(patch_size))/np.array(num_mask_units)).astype(int)
@@ -322,8 +328,220 @@ class HieraSeg(torch.nn.Module):
         )
 
     def forward(self, img):
-        features, _, _, _ = self.encoder(img)
+        breakpoint()
+        features, _, _, _, save_layers = self.encoder(img)
         features = rearrange(features, "b t c -> t b c")
         predicted_img = self.patch2img(features)
         predicted_img = self.upsample(predicted_img)
+        return predicted_img
+    
+
+
+
+class Mlp(nn.Module):
+    def __init__(
+        self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.0
+    ):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class CrossAttention(nn.Module):
+    def __init__(
+        self,
+        encoder_dim,
+        decoder_dim,
+        num_heads=8,
+        qkv_bias=False,
+        qk_scale=None,
+        attn_drop=0.0,
+        proj_drop=0.0,
+    ):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = decoder_dim // num_heads
+        self.scale = qk_scale or head_dim**-0.5
+        self.q = nn.Linear(decoder_dim, decoder_dim, bias=qkv_bias)
+        self.kv = nn.Linear(encoder_dim, decoder_dim * 2, bias=qkv_bias)
+        self.attn_drop = attn_drop
+        self.proj = nn.Linear(decoder_dim, decoder_dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x, y, mask):
+        B, N, C = x.shape
+        Ny = y.shape[1]
+        q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        kv = (
+            self.kv(y)
+            .reshape(B, Ny, 2, self.num_heads, C // self.num_heads)
+            .permute(2, 0, 3, 1, 4)
+        )
+        k, v = kv[0], kv[1]
+
+        attn = F.scaled_dot_product_attention(
+            q,
+            k,
+            v,
+            dropout_p=self.attn_drop,
+            attn_mask=mask>0.5 if mask is not None else None,
+        )
+        x = attn.transpose(1, 2).reshape(B, N, C)
+
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+from timm.models.vision_transformer import Attention
+
+class Mask2FormerBlock(nn.Module):
+    def __init__(
+        self,
+        encoder_dim,
+        decoder_dim,
+        num_heads,
+        mlp_ratio=4.0,
+        qkv_bias=False,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        act_layer=nn.GELU,
+        norm_layer=nn.LayerNorm,
+    ):
+        super().__init__()
+        self.norm1 = norm_layer(decoder_dim)
+
+        self.self_attn_block = Attention(
+            dim=decoder_dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+        )
+
+        self.cross_attn = CrossAttention(
+            encoder_dim,
+            decoder_dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+        )
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.norm2 = norm_layer(decoder_dim)
+        mlp_hidden_dim = int(decoder_dim * mlp_ratio)
+        self.mlp = Mlp(
+            in_features=decoder_dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop
+        )
+
+    def forward(self, x, y, mask):
+        """
+        x: query features, y: image features, mask: previous mask prediction
+        """
+        x = self.norm1(x + self.cross_attn(x, y, mask))
+        x = x + self.self_attn_block(x)
+        x = x + self.mlp(self.norm2(x))
+        return x
+
+class HieraMask2Former(torch.nn.Module):
+    def __init__(
+        self,
+        encoder_ckpt,
+        architecture,
+        spatial_dims: int = 3,
+        num_queries: int = 50,
+        num_patches: Optional[List[int]] = [2, 32, 32],
+        num_mask_units: Optional[List[int]] = [2, 12, 12],
+        patch_size: Optional[List[int]] = [16, 16, 16],
+        emb_dim: Optional[int] = 64,
+        context_pixels: Optional[List[int]] = [0, 0, 0],
+    ) -> None:
+        """
+        Parameters
+        ----------
+        """
+        super().__init__()
+        assert spatial_dims in (2, 3), "Spatial dims must be 2 or 3"
+
+        if isinstance(num_patches, int):
+            num_patches = [num_patches] * spatial_dims
+        if isinstance(patch_size, int):
+            patch_size = [patch_size] * spatial_dims
+
+        assert len(num_patches) == spatial_dims, "num_patches must be of length spatial_dims"
+        assert (
+            len(patch_size) == spatial_dims
+        ), "patch_size must be of length spatial_dims"
+        self.num_mask_units = num_mask_units
+
+        self.encoder = HieraEncoder(
+            num_patches=num_patches,
+            num_mask_units=num_mask_units,
+            architecture=architecture,
+            emb_dim=emb_dim,
+            spatial_dims=spatial_dims,
+            patch_size=patch_size,
+            mask_ratio=0,
+            context_pixels=context_pixels,
+        )
+        model = torch.load(encoder_ckpt, map_location="cuda:0")
+        enc_state_dict = {
+            k.replace("backbone.encoder.", ""): v
+            for k, v in model["state_dict"].items()
+            if "encoder" in k and "intermediate" not in k
+        }
+        self.encoder.load_state_dict(enc_state_dict, strict=False)
+        for name, param in self.encoder.named_parameters():
+            param.requires_grad = False
+
+        # "patches" to the decoder are actually mask units, so num_patches is num_mask_units, patch_size is mask unit size
+        mask_unit_size = ((np.array(num_patches) * np.array(patch_size))/np.array(num_mask_units)).astype(int)
+
+        self.instance_queries = torch.nn.Parameter(torch.zeros(1, num_queries, self.encoder.final_dim))
+        self.instance_queries_pos_emb = torch.nn.Parameter(torch.zeros(1, num_queries, self.encoder.final_dim))
+
+        q_strides = [np.array(stage['q_stride']) for stage in architecture if stage.get('q_stride', False)]
+        patches_per_mask_unit = [np.array(num_patches) // np.array(num_mask_units)]
+        for qs in q_strides:            
+            patches_per_mask_unit.append(patches_per_mask_unit[-1] // qs)
+        patches_per_mask_unit.reverse()
+        patches_per_mask_unit[0] = np.array([1,1,1])
+        self.patches_per_mask_unit = patches_per_mask_unit
+
+        # TODO each block should have a different embedding dimension
+        # self.transformer = torch.nn.ModuleList([Mask2FormerBlock() for _ in range(len(patches_per_mask_unit))])
+
+
+
+    def forward(self, img):
+        breakpoint()
+        #features are b x t x c
+        features, _, _, _, save_layers = self.encoder(img)
+        save_layers.append(features.unsqueeze(2))
+        save_layers.reverse()
+        # start with lowest resolution
+        mask = None
+        for layer, ppmu in zip(save_layers, self.patches_per_mask_unit):
+            # TODO add positional embedding and scale embedding to image features
+            # do we even need to rearrange to an image here if we're just doing cross attention against it?
+            img_features = rearrange(layer, 'b (n_mu_z n_mu_y n_mu_x) (patches_per_mu_z patches_per_mu_y patches_per_mu_x) c -> b c (n_mu_z patches_per_mu_z) (n_mu_y patches_per_mu_y) (n_mu_x patches_per_mu_x)', n_mu_z=self.num_mask_units[0], n_mu_y=self.num_mask_units[1], n_mu_x=self.num_mask_units[2], patches_per_mu_z=ppmu[0], patches_per_mu_y=ppmu[1], patches_per_mu_x=ppmu[2])
+
+
+            # mask = self.transformer(img_features, mask, self.instance_queries, self.instance_queries_pos_emb)
+
+
         return predicted_img
