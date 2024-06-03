c729091ec022e4ab00e7ca67e50018df7e2cfba6
Ritvik Vasan <ritvik.vasan@prd-aics-dcp-038.corp.alleninstitute.org>
2024-06-03 13:37:12 -0700

    remove topo losses

--- cyto_dl/nn/losses/__init__.py
+++ cyto_dl/nn/losses/__init__.py
@@ -7,11 +7,6 @@ from .gaussian_nll_loss import GaussianNLLLoss
 from .threshold_loss import ThresholdLoss
 from .weibull import WeibullLogLoss
 from .weighted_mse_loss import WeightedMSELoss
-from .topo_loss import TopoLoss
-from .chamfer_loss_self import ChamferLossSelf
-from .zerolevelset_loss import ZeroLevelSetLoss
-from .finetune_loss import FineTuneLoss
-from .finetune_loss2 import FineTuneLoss2
 
 try:
     from .spharm_loss import SpharmLoss
