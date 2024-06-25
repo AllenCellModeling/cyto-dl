4a223fb81cfe3ba8c32ab69f67d5b908e9c8c546
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-06-25 14:09:51 -0700

    add guard rails

--- cyto_dl/image/transforms/generate_jepa_masks.py
+++ cyto_dl/image/transforms/generate_jepa_masks.py
@@ -6,26 +6,35 @@ from typing import Tuple
 
 
 class JEPAMaskGenerator(RandomizableTransform):
-    def __init__(self, num_masks:int=4, mask_size:int=12, mask_aspect_ratio: Tuple[float]=(0.5,1.5), num_patches: Tuple[float]=(6, 24, 24), mask_ratio: float=0.9):
-        self.num_masks = num_masks
+    def __init__(self, mask_size:int=12, mask_aspect_ratio: Tuple[float]=(0.5,1.5), num_patches: Tuple[float]=(6, 24, 24), mask_ratio: float=0.9):
+        assert mask_ratio < 1, "mask_ratio must be less than 1"
+        assert mask_size * max(mask_aspect_ratio) < min(num_patches[-2:]), "mask_size * max mask aspect ratio must be less than the smallest dimension of num_patches"
+ 
         self.mask_size = mask_size
         self.mask_aspect_ratio = mask_aspect_ratio
         self.num_patches = num_patches
-        self.mask_ratio = mask_ratio
         self.target_pix = int(mask_ratio * np.prod(num_patches))
-        self.num_patches = num_patches
+        self.mask = np.zeros(num_patches)
+        self.edge_mask = np.ones(num_patches)
+        self.edge_mask[1:-1, 1:-1, 1:-1] = 0
 
     def __call__(self, img_dict):
         # generate context (small) and target(large) masks
-        mask = np.zeros(self.num_patches)
+        # target: add blocks until target_pix is reached, then randomly remove excess border pixels
+        # context: invert of target mask
+        mask = self.mask.copy()
         while mask.sum() < self.target_pix:
             aspect_ratio = np.random.uniform(*self.mask_aspect_ratio)
             width = int(self.mask_size*aspect_ratio)
             height = int(self.mask_size/aspect_ratio)
-            x = np.random.randint(0, self.num_patches[2]-width)
-            y = np.random.randint(0, self.num_patches[1]-height)
+            x = np.random.randint(0, self.num_patches[2]-width+1)
+            y = np.random.randint(0, self.num_patches[1]-height+1)
             mask[:, y:y+height, x:x+width] = 1
         bound = find_boundaries(mask, mode='inner')
+        # include image edge as boundary, not just 1:0 transitions
+        edge_mask = np.logical_and(mask, self.edge_mask)
+
+        bound = np.logical_or(bound, edge_mask)
         bound_coords = np.argwhere(bound)
         excess = int(mask.sum() - self.target_pix)
         remove = np.random.choice(range(bound_coords.shape[0]), excess, replace=False)
