4917984507f4c843466f703e2126779a5ddfc235
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-07-02 10:38:28 -0700

    remove non-jepa changes

--- cyto_dl/nn/vits/utils.py
+++ cyto_dl/nn/vits/utils.py
@@ -3,4 +3,6 @@ from einops import repeat
 
 
 def take_indexes(sequences, indexes):
-    return torch.gather(sequences, 0, repeat(indexes, "t b -> t b c", c=sequences.shape[-1]))
+    return torch.gather(
+        sequences, 0, repeat(indexes.to(sequences.device), "t b -> t b c", c=sequences.shape[-1])
+    )
