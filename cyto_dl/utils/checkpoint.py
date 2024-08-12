6d08d7b0d6a05ad63deff42af49cf5148a372ba1
Benjamin Morris <benjamin.morris@prd-aics-dcp-038.corp.alleninstitute.org>
2024-08-12 13:30:19 -0700

    precommit

--- cyto_dl/utils/checkpoint.py
+++ cyto_dl/utils/checkpoint.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def load_checkpoint(model, load_params):
     if load_params.get("weights_only"):
         assert load_params.get(
@@ -10,6 +11,6 @@ def load_checkpoint(model, load_params):
         model.load_state_dict(state_dict, strict=load_params.get("strict", True))
         # set ckpt_path to None to avoid loading checkpoint again with model.fit/model.test
         load_params["ckpt_path"] = None
-    elif not load_params.get('strict'):
+    elif not load_params.get("strict"):
         raise ValueError("To use `strict=False`, `weights_only` must be set to True")
     return model, load_params
