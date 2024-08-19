71b80969fd9e352c6a78084ddf8f3105a8d67cd7
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-08-19 13:24:39 -0700

    2d masked unit attention

--- cyto_dl/nn/vits/blocks/masked_unit_attention.py
+++ cyto_dl/nn/vits/blocks/masked_unit_attention.py
@@ -9,12 +9,15 @@ from einops import rearrange, reduce
 from einops.layers.torch import Reduce
 from timm.models.layers import DropPath, Mlp
 
+from cyto_dl.nn.vits.utils import validate_spatial_dims
+
 
 class MaskUnitAttention(torch.nn.Module):
     def __init__(
         self,
         dim,
         dim_out,
+        spatial_dims: int = 3,
         num_heads=8,
         qkv_bias=False,
         qk_scale=None,
@@ -23,7 +26,36 @@ class MaskUnitAttention(torch.nn.Module):
         q_stride=[1, 1, 1],
         patches_per_mask_unit=[2, 12, 12],
     ):
+        """
+        Parameters
+        ----------
+        dim : int
+            Dimension of the input features.
+        dim_out : int
+            Dimension of the output features.
+        spatial_dims : int, optional
+            Number of spatial dimensions, by default 3.
+        num_heads : int, optional
+            Number of attention heads, by default 8.
+        qkv_bias : bool, optional
+            If True, add a learnable bias to query, key, value, by default False.
+        qk_scale : float, optional
+            Override default qk scale of head_dim ** -0.5 if set, by default None.
+        attn_drop : float, optional
+            Dropout rate for attention, by default 0.0.
+        proj_drop : float, optional
+            Dropout rate for projection, by default 0.0.
+        q_stride : List[int], optional
+            Stride for query, by default [1, 1, 1].
+        patches_per_mask_unit : List[int], optional
+            Number of patches per mask unit, by default [2, 12, 12].
+        """
         super().__init__()
+        q_stride, patches_per_mask_unit = validate_spatial_dims(
+            spatial_dims, [q_stride, patches_per_mask_unit]
+        )
+
+        self.spatial_dims = spatial_dims
         self.num_heads = num_heads
         self.head_dim = dim_out // num_heads
         self.scale = qk_scale or self.head_dim**-0.5
@@ -41,7 +73,7 @@ class MaskUnitAttention(torch.nn.Module):
         # project and split into q,k,v embeddings
         qkv = rearrange(
             self.qkv(x),
-            "batch num_mask_units tokens_per_mask_unit (head_dim num_heads qkv) -> qkv batch num_mask_units num_heads tokens_per_mask_unit  head_dim",
+            "batch num_mask_units tokens_per_mask_unit (head_dim num_heads qkv) -> qkv batch num_mask_units num_heads tokens_per_mask_unit head_dim",
             head_dim=self.head_dim,
             qkv=3,
             num_heads=self.num_heads,
@@ -52,17 +84,28 @@ class MaskUnitAttention(torch.nn.Module):
         if np.any(self.q_stride > 1):
             # within a mask unit, tokens are spatially ordered
             # perform spatial 2x2x2 max pooling over tokens
-            q = reduce(
-                q,
-                "b n h (n_patches_z q_stride_z n_patches_y q_stride_y n_patches_x q_stride_x) c ->b n h (n_patches_z n_patches_y n_patches_x) c",
-                reduction="max",
-                q_stride_z=self.q_stride[0],
-                q_stride_y=self.q_stride[1],
-                q_stride_x=self.q_stride[2],
-                n_patches_z=self.pooled_patches_per_mask_unit[0],
-                n_patches_y=self.pooled_patches_per_mask_unit[1],
-                n_patches_x=self.pooled_patches_per_mask_unit[2],
-            )
+            if self.spatial_dims == 3:
+                q = reduce(
+                    q,
+                    "b n h (n_patches_z q_stride_z n_patches_y q_stride_y n_patches_x q_stride_x) c ->b n h (n_patches_z n_patches_y n_patches_x) c",
+                    reduction="max",
+                    q_stride_z=self.q_stride[0],
+                    q_stride_y=self.q_stride[1],
+                    q_stride_x=self.q_stride[2],
+                    n_patches_z=self.pooled_patches_per_mask_unit[0],
+                    n_patches_y=self.pooled_patches_per_mask_unit[1],
+                    n_patches_x=self.pooled_patches_per_mask_unit[2],
+                )
+            elif self.spatial_dims == 2:
+                q = reduce(
+                    q,
+                    "b n h (n_patches_y q_stride_y n_patches_x q_stride_x) c ->b n h (n_patches_y n_patches_x) c",
+                    reduction="max",
+                    q_stride_y=self.q_stride[0],
+                    q_stride_x=self.q_stride[1],
+                    n_patches_y=self.pooled_patches_per_mask_unit[0],
+                    n_patches_x=self.pooled_patches_per_mask_unit[1],
+                )
 
         attn = F.scaled_dot_product_attention(
             q,
@@ -86,6 +129,7 @@ class HieraBlock(nn.Module):
         dim: int,
         dim_out: int,
         heads: int,
+        spatial_dims: int = 3,
         mlp_ratio: float = 4.0,
         drop_path: float = 0.0,
         norm_layer: nn.Module = nn.LayerNorm,
@@ -93,8 +137,36 @@ class HieraBlock(nn.Module):
         q_stride: List[int] = [1, 1, 1],
         patches_per_mask_unit: List[int] = [2, 12, 12],
     ):
+        """
+        Parameters
+        ----------
+        dim : int
+            Dimension of the input features.
+        dim_out : int
+            Dimension of the output features.
+        spatial_dims : int, optional
+            Number of spatial dimensions, by default 3.
+        num_heads : int, optional
+            Number of attention heads, by default 8.
+        qkv_bias : bool, optional
+            If True, add a learnable bias to query, key, value, by default False.
+        qk_scale : float, optional
+            Override default qk scale of head_dim ** -0.5 if set, by default None.
+        attn_drop : float, optional
+            Dropout rate for attention, by default 0.0.
+        proj_drop : float, optional
+            Dropout rate for projection, by default 0.0.
+        q_stride : List[int], optional
+            Stride for query, by default [1, 1, 1].
+        patches_per_mask_unit : List[int], optional
+            Number of patches per mask unit, by default [2, 12, 12].
+        """
         super().__init__()
+        patches_per_mask_unit, q_stride = validate_spatial_dims(
+            spatial_dims, [patches_per_mask_unit, q_stride]
+        )
 
+        self.spatial_dims = spatial_dims
         self.dim = dim
         self.dim_out = dim_out
         self.q_stride = q_stride
@@ -106,6 +178,7 @@ class HieraBlock(nn.Module):
         self.attn = MaskUnitAttention(
             dim,
             dim_out,
+            spatial_dims=spatial_dims,
             num_heads=heads,
             q_stride=q_stride,
             patches_per_mask_unit=patches_per_mask_unit,
@@ -117,16 +190,26 @@ class HieraBlock(nn.Module):
         self.drop_path = DropPath(drop_path) if drop_path > 0 else nn.Identity()
 
         # max pooling by q stride within a mask unit
-        skip_connection_pooling = Reduce(
-            "b n (n_patches_z q_stride_z n_patches_y q_stride_y n_patches_x q_stride_x) c -> b n (n_patches_z n_patches_y n_patches_x) c",
-            reduction="mean",
-            q_stride_z=self.q_stride[0],
-            q_stride_y=self.q_stride[1],
-            q_stride_x=self.q_stride[2],
-            n_patches_z=self.attn.pooled_patches_per_mask_unit[0],
-            n_patches_y=self.attn.pooled_patches_per_mask_unit[1],
-            n_patches_x=self.attn.pooled_patches_per_mask_unit[2],
-        )
+        if self.spatial_dims == 3:
+            skip_connection_pooling = Reduce(
+                "b n (n_patches_z q_stride_z n_patches_y q_stride_y n_patches_x q_stride_x) c -> b n (n_patches_z n_patches_y n_patches_x) c",
+                reduction="mean",
+                q_stride_z=self.q_stride[0],
+                q_stride_y=self.q_stride[1],
+                q_stride_x=self.q_stride[2],
+                n_patches_z=self.attn.pooled_patches_per_mask_unit[0],
+                n_patches_y=self.attn.pooled_patches_per_mask_unit[1],
+                n_patches_x=self.attn.pooled_patches_per_mask_unit[2],
+            )
+        elif self.spatial_dims == 2:
+            skip_connection_pooling = Reduce(
+                "b n (n_patches_y q_stride_y n_patches_x q_stride_x) c -> b n (n_patches_y n_patches_x) c",
+                reduction="mean",
+                q_stride_y=self.q_stride[0],
+                q_stride_x=self.q_stride[1],
+                n_patches_y=self.attn.pooled_patches_per_mask_unit[0],
+                n_patches_x=self.attn.pooled_patches_per_mask_unit[1],
+            )
 
         self.proj = (
             torch.nn.Sequential(skip_connection_pooling, nn.Linear(dim, dim_out))
