419bf7952b2bfe1dc48d8440ca6c77056bdf6b1c
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2025-03-11 12:57:06 -0700

    remove metatensor utils

--- cyto_dl/models/utils/utils.py
+++ cyto_dl/models/utils/utils.py
@@ -10,11 +10,3 @@ def find_indices(lst, vals):
         indices = set(np.where(arr == val)[0] - i)
         sets.append(indices)
     return np.asarray(list(set.intersection(*sets)), dtype=int)
-
-
-def metatensor_batch_to_tensor(batch):
-    """Convert monai metatensors to tensors."""
-    for k, v in batch.items():
-        if isinstance(v, MetaTensor):
-            batch[k] = v.as_tensor()
-    return batch
